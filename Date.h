92c8a49b2d72f3ca5c867919744f422138abef8a
Ashly Hernandez <ashlyhernandez25@gmail.com>
2016-02-24 20:53:39 -0800

    Added files via upload

---
+++ Date.h
@@ -0,0 +1,38 @@
+/*
+ * Date.h
+ *
+ *  Created on: Feb 24, 2016
+ *      Author: ashlyhernandez
+ */
+
+#ifndef DATE_H_
+#define DATE_H_
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Date {
+public:
+
+	Date();
+	Date(int monthIn, int dayIn, int yeaerIn);
+
+	virtual ~Date();
+
+	void UpdateDate(int newMonth, int newDay, int newYear);
+	void PrintDate() const;
+	int GetMonth() const;
+	int GetDay() const;
+	int Getyear() const;
+
+
+
+
+private:
+	int month;
+	int day;
+	int year;
+};
+
+#endif /* DATE_H_ */
