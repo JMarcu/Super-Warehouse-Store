f283d3031ff8ca6b0bd185aa3f8c4674f3711a64
jmarcu0 <jmarcu0@L-SM-346-21D.student.saddleback.edu>
2016-02-24 16:31:44 -0800

    Updated Database Interface. Report interfaces to follow.

--- Database.h
+++ Database.h
@@ -8,7 +8,36 @@
 #ifndef DATABASE_H_
 #define DATABASE_H_
 
+#include <string>
+#include <list>
+#include "Reports.h"
+#include "Date.h"
+using namespace std;
 
+class Database{
+private:
+
+public:
+	//Reports
+	DailySalesReport getDailySalesReport(Date day) const;
+	TotalPurchaseReport getTotalPurchaseReport() const;
+	TotalItemReport getTotalItemReport() const;
+	RebateReport getRebateReport() const;
+	ExpirationReport getExpirationReport(Date month) const;
+	ItemReport getItemReport(const Item& item) const;
+	MemberPurchaseReport getMemberPurchaseReport(int memberID) const;
+	MemberPurchaseReport getMemberPurchaseReport(string memberName) const;
+	RegularConversionReport getRegularConversionReport() const;
+	ExecutiveConversionReport getExecutiveConversionReport() const;
+
+	//Modifiers
+	void AddMember(const Member& member);
+	void DeleteMember(const Member& member);
+	void AddItem(const Item& item);
+	void DeleteItem(const Item& item);
+	void AddSale(const Sale& sale);
+	void DeleteSale(const Sale& sale);
+};
 
 
 
