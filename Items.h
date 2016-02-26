f33c9da88aeb5d0be40011c35ff04300e31d436a
JaredRauch <jdrauch95@me.com>
2016-02-25 16:56:13 -0800

    Create Items.h
    This class is planned to be used as simple for the client as possible.  However, feel free to edit this code in case of any circumstances with the client code.  I'll will also provide a .cpp file for the Methods' and Accessors' definitions.

---
+++ Items.h
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+class Item
+{
+	public:
+	
+	// Constructors/Destructors
+	Items();
+	Items(string name, int amountz, double money);
+	~Items();
+
+//   Methods(Mutators)
+//   -----------------
+	void SetItem(string name);
+
+	void SetItemProfile(string name, int amountz, double money);
+
+	void SetAmount(int amountz);
+
+	void SetPrice(double money);
+
+	
+
+//  Accessors
+// --------------
+	string GetItem();
+
+	int GetAmount();
+
+	double GetPrice(); 
+
+	
+	private:
+	
+//  Data members(variables)
+// -------------------------
+
+	string itemName;
+
+	int quantity;
+
+	double price;	
+}
