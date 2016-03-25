10933117043b63c69be9c75be07df29a0de4376f
JaredRauch <jdrauch95@me.com>
2016-03-24 23:03:55 -0700

    Added files via upload
    Here are the modified files that has the code based from the date combo box algorithm.  Feel free to copy and paste the code from these files or merge them upon merging request.

--- editmemberwindow.cpp
+++ editmemberwindow.cpp
@@ -20,7 +20,7 @@ void editMemberWindow::on_editMemberConform_accepted()
     memberName = ui->memberEditName->text();
 
     expirationDate.UpdateDate(ui->CB_Month->currentIndex(), ui->CB_Day->currentIndex(),
-                          ui->CB_year->currentIndex());
+                              ui->CB_year->currentIndex());
 
     if(ui->comboBoxExecutive->currentIndex() == 0)
     {
@@ -44,3 +44,98 @@ void editMemberWindow::on_editMemberConform_rejected()
     hide();
 }
 
+/*******************************DATE COMBOBOX ALGORITHM HERE***************************/
+void editMemberWindow::on_CB_Month_currentIndexChanged(int index)
+{
+    int leap = ui->CB_year->currentText().toInt(0,10) % 4;
+
+    if(ui->CB_Month->currentIndex() == 1 || ui->CB_Month->currentIndex() == 3 ||
+       ui->CB_Month->currentIndex() == 5 || ui->CB_Month->currentIndex() == 7 ||
+       ui->CB_Month->currentIndex() == 8 || ui->CB_Month->currentIndex() == 10
+       || ui->CB_Month->currentIndex() == 12)
+    {
+        if(ui->CB_Day->maxCount() < 32)
+        {
+            if(ui->CB_Day->maxCount() < 31)
+            {
+                if(ui->CB_Day->maxCount() < 30)
+                {
+                    ui->CB_Day->setMaxCount(32);
+                    ui->CB_Day->addItem("29");
+                    ui->CB_Day->addItem("30");
+                    ui->CB_Day->addItem("31");
+                }
+                else
+                {
+                    ui->CB_Day->setMaxCount(32);
+                    ui->CB_Day->addItem("30");
+                    ui->CB_Day->addItem("31");
+                }
+            }
+            else
+            {
+                ui->CB_Day->setMaxCount(32);
+                ui->CB_Day->addItem("31");
+            }
+        }
+    }
+    else if(ui->CB_Month->currentIndex() == 2)
+    {
+        if(leap == 0 || ui->CB_year->currentIndex() == 1)
+        {
+           ui->CB_Day->setMaxCount(30);
+
+        }
+        else
+        {
+           ui->CB_Day->setMaxCount(29);
+        }
+    }
+    else
+    {
+        ui->CB_Day->setMaxCount(31);
+    }
+}
+
+void editMemberWindow::on_CB_year_currentIndexChanged(int index)
+{
+    int leapyear = ui->CB_year->currentText().toInt(0,10) % 4;
+
+    if(ui->CB_Day->maxCount() < 32)
+    {
+        if(ui->CB_Day->maxCount() < 31)
+        {
+            if(ui->CB_Day->maxCount() < 30)
+            {
+                ui->CB_Day->setMaxCount(32);
+                ui->CB_Day->addItem("29");
+                ui->CB_Day->addItem("30");
+                ui->CB_Day->addItem("31");
+            }
+            else
+            {
+                ui->CB_Day->setMaxCount(32);
+                ui->CB_Day->addItem("30");
+                ui->CB_Day->addItem("31");
+            }
+        }
+        else
+        {
+            ui->CB_Day->setMaxCount(32);
+            ui->CB_Day->addItem("31");
+        }
+    }
+
+    if(ui->CB_Month->currentIndex() == 2)
+    {
+        if(ui->CB_year->currentIndex() == 1 && leapyear == 0)
+        {
+            ui->CB_Day->setMaxCount(30);
+        }
+        else
+        {
+            ui->CB_Day->setMaxCount(29);
+        }
+    }
+}
+/*************************************************************************************/
